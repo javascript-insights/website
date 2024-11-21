4c1bcfbafe71d3d6fec4ff039f349b5ba7985aed
Idan Mor <sidanmor@gmail.com>
2024-11-21 10:38:12 +0200

    ADD sources

---
+++ devtools/console/theoretical-3/index.js
@@ -0,0 +1,25 @@
+window.addEventListener("load", function () {
+    // prints the text to the console as  a log message
+    console.log('This is a log message from Console Theoretical 3');
+
+    // prints the text to the console as an informational message
+    console.info('This is some information from Console Theoretical 3');
+
+    // prints the text to the console as an error message
+    console.error('This is an error from Console Theoretical 3');
+
+    // prints the text to the console as a warning
+    console.warn('This is a warning from Console Theoretical 3');
+
+    // prints the geometry of the document body as an object
+    console.log(document.body.getBoundingClientRect(), 'from Console Theoretical 3');
+
+    // prints the geometry of the document body as a table
+    console.table(document.body.getBoundingClientRect(), 'from Console Theoretical 3');
+
+    // shows a list of techologies as a collapsed group
+    let technologies = ["HTML", "CSS", "SVG", "ECMAScript"];
+    console.groupCollapsed('Technolgies');
+    technologies.forEach(tech => { console.info(tech); })
+    console.groupEnd('Technolgies');
+});
--- devtools/elements/theoretical/index.js
+++ devtools/elements/theoretical/index.js
@@ -4,28 +4,4 @@ window.addEventListener("load", function () {
   noButton.addEventListener("mouseover", () => {
     noButton.setAttribute("style", `position: absolute; left: ${Math.random() * 200}px; top: ${Math.random() * 200}px`);
   });
-
-  // prints the text to the console as  a log message
-  console.log('This is a log message from Elements Theoretical');
-
-  // prints the text to the console as an informational message
-  console.info('This is some information from Elements Theoretical');
-
-  // prints the text to the console as an error message
-  console.error('This is an error from Elements Theoretical');
-
-  // prints the text to the console as a warning
-  console.warn('This is a warning from Elements Theoretical');
-
-  // prints the geometry of the document body as an object
-  console.log(document.body.getBoundingClientRect(), 'from Elements Theoretical');
-
-  // prints the geometry of the document body as a table
-  console.table(document.body.getBoundingClientRect(), 'from Elements Theoretical');
-
-  // shows a list of techologies as a collapsed group
-  let technologies = ["HTML", "CSS", "SVG", "ECMAScript"];
-  console.groupCollapsed('Technolgies');
-  technologies.forEach(tech => { console.info(tech); })
-  console.groupEnd('Technolgies');
 });
