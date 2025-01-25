76e306ebe486e36c62c314922bc0ce9bba9759ef
Idan Mor <sidanmor@gmail.com>
2025-01-25 23:09:44 +0200

    Add geolocation feature to Sensors tool with interactive map display

--- devtools/moretools/sensors/theoretical-1/index.js
+++ devtools/moretools/sensors/theoretical-1/index.js
@@ -1,2 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
+
 });
+
+function getLocation() {
+    const demo = document.getElementById("demo");
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        demo.innerHTML = "Geolocation is not supported by this browser.";
+    }
+
+}
+
+function showPosition(position) {
+    const demo = document.getElementById("demo");
+    demo.innerHTML = "Latitude: " + position.coords.latitude +
+        "<br>Longitude: " + position.coords.longitude;
+
+    const src = `https://maps.google.com/maps?hl=en&q=${position.coords.latitude},${position.coords.longitude}&output=embed`;
+    const demoIframe = document.getElementById("demoIframe");
+    demoIframe.src = src;
+
+
+}
+
